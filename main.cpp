967d0efe20ed446ee3530e3aafb03b7a87d2247c
edencio-w <55332130+edencio-w@users.noreply.github.com>
2019-09-14 16:55:19 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//#includes are ALWAYS at top of file
+#include <iostream>	//iostream is where std::cout and std::endl are defined
+#include <string.h>
+#include "myfunc.h"
+
+
+
+int main()
+{
+	std::string a = myfunc();
+	std::cout<< a <<std::endl;
+	return 0;
+}
